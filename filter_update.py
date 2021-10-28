6bdb9f570fc38226a224a5bdde0bcce2313c62e2
Поддубко Сергей <snpod@rambler.ru>
2021-10-28 21:31:39 +1000

    client_api

--- filter_update.py
+++ filter_update.py
@@ -10,16 +10,19 @@ list_of_chats = [-632320349, -1001424513919, ]
 TELEGRAM_TOKEN = os.getenv('TOKEN')
 API_HASH = os.getenv('API_HASH')
 API_ID = os.getenv('API_ID')
+local_dir = os.path.abspath(os.curdir)
 
 app = Client('my_account', API_ID, API_HASH)
 
 CHAT_IDS = os.getenv('CHAT_IDS')
 CHAT_IDS = [int(el) for el in CHAT_IDS.split(',') if el]
-template_of_numbers = [3421, 6079, 594, 595, 2353, 4824, 4556, 21, 17, 2354,
-                       4825, 60, 637, 2355, 1459, 2255, 4036, 70, 66, 60,
-                       6229, 3429, 645, 5356, 2350, 4530, 4559, 4558, 4557,
-                       2825, 2254, 3797, 5486, 64]
-
+with open(local_dir+'/data', 'r') as f:
+    numbers_str = f.read()
+template_of_numbers = [int(el) for el in numbers_str.split(', ')]
+# template_of_numbers = [3421, 6079, 594, 595, 2353, 4824, 4556, 21, 17, 2354,
+#                        4825, 60, 637, 2355, 1459, 2255, 4036, 70, 66, 60,
+#                        6229, 3429, 645, 5356, 2350, 4530, 4559, 4558, 4557,
+#                        2825, 2254, 3797, 5486, 64]
 try:
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
 except Exception as error:
